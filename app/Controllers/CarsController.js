ec4723fddbc8c24e501bff444ffd793c8bfb2f7b
Timothy Kimble <timble417@gmail.com>
2021-07-20 11:59:32 -0600

    axios cars

--- app/Controllers/CarsController.js
+++ app/Controllers/CarsController.js
@@ -1,4 +1,5 @@
 import { ProxyState } from "../AppState.js"
+import { api } from "../Services/AxiosService.js"
 import { carsService } from "../Services/CarsService.js"
 
 function _draw() {
@@ -19,18 +20,34 @@ export default class CarsController {
     _draw()
   }
 
-  createCar() {
-    event.preventDefault()
-    let form = event.target
-    let rawCar = {
-      make: form.make.value,
-      model: form.model.value,
-      year: form.year.value,
-      price: form.price.value,
-      description: form.description.value,
-      imgUrl: form.imgUrl.value
+  async createCar() {
+    try {
+      event.preventDefault()
+      let form = event.target
+      let rawCar = {
+        make: form.make.value,
+        model: form.model.value,
+        year: form.year.value,
+        price: form.price.value,
+        description: form.description.value,
+        imgUrl: form.imgUrl.value
+      }
+      await carsService.createCar(rawCar)
+      form.reset()
+    } catch (error) {
+      console.error(error)
+      window.alert(error.message)
     }
-    carsService.createCar(rawCar)
-    form.reset()
+  }
+
+
+  deleteCar(carId) {
+    console.log('you are trying to delete the car by the id of ' + carId)
+    carsService.deleteCar(carId)
+  }
+
+  async bidCar(carId) {
+    console.log('you are biding on the car with the id of', carId)
+    carsService.bidCar(carId)
   }
 }
